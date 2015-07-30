f2fb7242760e204d4278a5be4febb9714327a048
ryankapur <ryankapur@berkeley.edu>
2015-07-30 16:43:14 -0700

    color change

--- dollhouse/objVisibility.js
+++ dollhouse/objVisibility.js
@@ -1,14 +1,38 @@
-//TODO: intialize stuff as invisible
-
 function makeVisible(component, bool) {
-	findObjectsById(component).forEach(function(item) {
-    	item.traverse(function (object) {
-	        if (bool) {
-	        	object.visible = true;
-	        }
-	        else {
-	        object.visible = false;
-	        }
-        });
-    });
+	try {
+		var list = findObjectsById(component);
+		for (item in list){
+			item.traverse(function (object) {
+			    if (bool) {
+			        object.visible = true;
+			    }
+			    else {
+			        object.visible = false;
+			    }
+		    });
+
+		}
+
+		findObjectsById(component).forEach(function(item) {
+		    item.traverse(function (object) {
+			    if (bool) {
+			        object.visible = true;
+			    }
+			    else {
+			        object.visible = false;
+			    }
+		    });
+		});
+	}
+	catch (err) {
+                //controlLine('TypeError caught1');
+
+	}
+}
+
+
+//intialize every item as invisible
+var components = ['_445CTmine', 'security-system_motion-detector', 'Component_36', 'Component_35', 'Component_34', 'Component_33'];
+for (entry in components){
+ makeVisible(entry, false)
 }
